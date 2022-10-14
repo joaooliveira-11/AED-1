37ad6b04a18b9485d90ae2028da84e72f319a5e1
andrensenn <andrensenn@gmail.com>
2022-10-14 10:51:28 +0100

    inicio do menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    bool flag = true;
+    char tecla;
+    while (flag){
+        cout << ("Press a  key: n\ 1 : n\ Q : Quit");
+        cin >> tecla;
+        switch (tecla) {
+            case "1":
+                //fará algo
+                break;
+            case "Q":
+                flag = false;
+                break;
+            default:
+                cout << (" Press a valid key.")
+                break;
+        }
+    }
 }
