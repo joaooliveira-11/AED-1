f1379a760508efdb0910cccedb6067d28f275d0f
andrensenn <andrensenn@gmail.com>
2022-10-14 10:54:36 +0100

    Menu funcional

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@ int main() {
     bool flag = true;
     char tecla;
     while (flag){
-        cout << ("Press a  key: n\ 1 : n\ Q : Quit");
+        cout << ("Press a  key: \n 1 : \n Q : Quit \n");
         cin >> tecla;
         switch (tecla) {
-            case "1":
+            case '1':
                 //fará algo
                 break;
-            case "Q":
+            case 'q':
                 flag = false;
                 break;
             default:
-                cout << (" Press a valid key.")
+                cout << (" Press a valid key.");
                 break;
         }
     }
