3d9a6d8e5b635ca7a9c175f8bca4ab2bb1b5a39e
andrensenn <andrensenn@gmail.com>
2022-10-15 11:28:00 +0100

    Student.cpp updated

--- classes/Student.cpp
+++ classes/Student.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Student.h"
+
+using namespace std;
+
+class Student{
+
+    void Student::Student(int code, string name, Schedule schedule){
+        studentCode = code;
+        studentName = name;
+        studentSchedule = schedule;
+    }
+
+    void Student::setSchedule(Schedule schedule){
+        studentSchedule = schedule;
+    }
+};
