dc74d56911e3a670dcc2e7b2d88091de56d70ebe
andrensenn <andrensenn@gmail.com>
2022-11-02 16:45:54 +0000

    Atualiza o numero de alunos apos a execução da queueueueue

--- classes/Turma.cpp
+++ classes/Turma.cpp
@@ -10,9 +10,12 @@ Turma::Turma(string Classcode, string Uccode, int numeroAlunos){
 string Turma::get_classcode() {return Classcode;}
 string Turma::get_uccode() {return Uccode;}
 int Turma::get_numeroalunos() const {return numeroAlunos;}
+void Turma::set_numeroalunos_0() {this->numeroAlunos=0;}
 
 void Turma::adder() {this->numeroAlunos = this->get_numeroalunos() + 1;}
 
+void Turma::sub() {this->numeroAlunos = this->get_numeroalunos() - 1;}
+
 bool Turma::can_add(map<string, int> Max_students_by_UC){
-    return (Max_students_by_UC[this->Uccode] - this->numeroAlunos+1 >= 4);
+    return (Max_students_by_UC[this->Uccode] >= this->numeroAlunos+1);
 }
