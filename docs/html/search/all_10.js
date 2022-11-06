37c51560d5c8ed66e2c70068f3bd14b520476d46
Diana Martins <iamdianamartins>
2022-11-06 15:31:36 +0000

    more documentation files added

---
+++ docs/html/search/all_10.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['verificar_0',['verificar',['../class_aluno.html#a2711adce0727e58a8363f74ee6b88a4b',1,'Aluno']]],
+  ['view_1',['view',['../class_bst.html#afbb8010ba4bd8d2976e7b7692988786d',1,'Bst']]],
+  ['view_5fby_5fnucs_2',['view_by_NUCS',['../class_bst.html#a5167bba895f62ce55d0f9efe6e7ec97b',1,'Bst']]],
+  ['view_5fby_5fturma_3',['view_by_turma',['../class_bst.html#a8ce39be8292b134329d721df75eaf295',1,'Bst']]],
+  ['view_5fby_5fuc_4',['view_by_uc',['../class_bst.html#a25f3a5d5f334fc42b679659c13be9f77',1,'Bst']]]
+];
---
+++ docs/html/search/functions_10.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['writedown_0',['writeDown',['../class_reading.html#a0c7ca83db684d79db52882956722a18c',1,'Reading']]]
+];
